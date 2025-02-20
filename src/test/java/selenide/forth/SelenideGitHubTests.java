48ca90a93af9fcd25ed0f68ee6a7aec2222b6115
peppbaggio <melkijbes@gmail.com>
2025-02-20 23:57:49 +0300

    Small refactoring in SelenideGithub.java

--- src/test/java/selenide/forth/SelenideGitHubTests.java
+++ src/test/java/selenide/forth/SelenideGitHubTests.java
@@ -1,4 +1,4 @@
-package selenium.forth;
+package selenide.forth;
 
 import com.codeborne.selenide.Configuration;
 
@@ -18,7 +18,7 @@ static void configSetup() {
 }
 
 @Test
-    void checkSelenidePage() {
+    void checkSelenidePageTest() {
 
     open("/selenide/selenide");
     $("#wiki-tab").click();
