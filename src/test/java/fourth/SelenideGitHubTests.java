925ecd1a2eef94f8505580d547c0427e145c7cc3
peppbaggio <melkijbes@gmail.com>
2025-02-20 22:19:49 +0300

    Some refactring in SelenideGithub.java

--- src/test/java/fourth/SelenideGitHubTests.java
+++ src/test/java/fourth/SelenideGitHubTests.java
@@ -3,7 +3,7 @@ package fourth;
 import com.codeborne.selenide.Configuration;
 
 import static com.codeborne.selenide.Condition.*;
-import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selectors.*;
 import static com.codeborne.selenide.Selenide.*;
 
 import org.junit.jupiter.api.BeforeAll;
@@ -22,11 +22,22 @@ static void configSetup() {
 
     open("/selenide/selenide");
     $("#wiki-tab").click();
-    $(byText("Soft assertions")).click();
+    $(byText("Soft assertions")).shouldBe(visible).click();
 
-    $$("h4").findBy(text("3. Using JUnit5 extend test class:")).shouldBe(visible);
-}
+    $$("div h4").findBy(text("3. Using JUnit5 extend test class:")).shouldBe(visible);
 
+    $$("pre").findBy(text("@ExtendWith({SoftAssertsExtension.class})\n" +
+            "class Tests {\n" +
+            "  @Test\n" +
+            "  void test() {\n" +
+            "    Configuration.assertionMode = SOFT;\n" +
+            "    open(\"page.html\");\n" +
+            "\n" +
+            "    $(\"#first\").should(visible).click();\n" +
+            "    $(\"#second\").should(visible).click();\n" +
+            "  }\n" +
+            "}")).scrollIntoView(true) .shouldBe(visible);
 
+        }
 
 }
