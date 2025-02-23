66565f282f013b0ec86b7abce2a9ae1eee48f1b5
peppbaggio <melkijbes@gmail.com>
2025-02-23 22:03:37 +0300

    Added DragNDropTest.java test

---
+++ src/test/java/selenide/fifth/DragNDropTest.java
@@ -0,0 +1,35 @@
+package selenide.fifth;
+
+import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.DragAndDropOptions;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selenide.*;
+
+public class DragNDropTest {
+
+    @BeforeAll
+    static void setConfig() {
+        Configuration.baseUrl = "https://the-internet.herokuapp.com";
+        Configuration.pageLoadStrategy = "eager";
+    }
+
+    @Test
+    void dragNDropTest () {
+        open("/drag_and_drop");
+
+        actions().moveToElement($("#column-a")).clickAndHold()
+                .moveToElement($("#column-b")).release().perform();
+        $("#column-a header").shouldHave(text("B"));
+    }
+
+    @Test
+    void dragNDropElementTest () {
+        open("/drag_and_drop");
+        $("#column-a").dragAndDrop(DragAndDropOptions.to("#column-b"));
+        $("#column-a header").shouldHave(text("B"));
+    }
+
+}
